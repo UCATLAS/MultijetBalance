c07570eaed33e5dbf02437e6f623f2c7c3bfab62
Jeff Dandoy <Jeff.Dandoy@cern.ch>
2016-07-29 15:36:56 -0500

    Changes

--- MultijetBalance/MultijetBalanceAlgo.h
+++ MultijetBalance/MultijetBalanceAlgo.h
@@ -49,41 +49,71 @@ class SystContainer;
 /**
     @brief Event selection of the Multijet Balance
  */
+
+// variables that don't get filled at submission time should be
+// protected from being send from the submission node to the worker
+// node (done by the //!)
 class MultijetBalanceAlgo : public EL::Algorithm
 {
   // put your configuration variables here as public variables.
   // that way they can be set directly from CINT and python.
   public:
 
+    /** @brief TEvent object */
     xAOD::TEvent *m_event;  //!
+    /** @brief TStore object for variable storage*/
     xAOD::TStore *m_store;  //!
+    /** @brief Count of the current event*/
     int m_eventCounter;     //!
-
+    /** @brief Global name to give to the algorithm*/
     std::string m_name;
-    std::string m_configName;
+
+//No longer used:    std::string m_configName;
+    /** @brief */
     float m_mcEventWeight; //!
+    /** @brief */
     std::string m_comEnergy; //!
 
     // configuration variables set by user
+    /** @brief */
     std::string m_inContainerName;        // input container name
+    /** @brief */
     std::string m_triggerAndPt;
+    /** @brief */
     int m_MJBIteration;               // Number of previous MJB iterations
+    /** @brief */
     std::string m_MJBIterationThreshold;
+    /** @brief */
     std::string m_MJBCorrectionFile;
+    /** @brief */
     std::string m_MJBBinningName;
+    /** @brief */
     std::string m_sysVariations;                  // Systematic Variations to use
+    /** @brief */
     bool m_MJBStatsOn;
+    /** @brief */
     unsigned int m_numJets;
+    /** @brief */
     float m_ptAsym;
+    /** @brief */
     float m_alpha;
+    /** @brief */
     float m_beta;
+    /** @brief */
     float m_ptThresh;
+    /** @brief */
     bool m_allJetBeta;
+    /** @brief */
     bool m_bootstrap;
+    /** @brief */
     bool m_leadingInsitu;
+    /** @brief */
     bool m_noLimitJESPt;
+    /** @brief */
     bool m_closureTest;
+    /** @brief */
     bool m_leadJetMJBCorrection;
+    /** @brief */
     bool m_reverseSubleading;
     /** @brief True value will allow TTree output */
     bool m_writeTree;
@@ -110,40 +140,66 @@ class MultijetBalanceAlgo : public EL::Algorithm
     /** @brief Option to output the cutflow histograms */
     bool m_useCutFlow;
 
+    /** @brief */
     int m_systTool_nToys;
+    /** @brief */
     std::string m_binning;
+    /** @brief */
     std::string m_VjetCalibFile;
 
+    /** @brief */
     bool m_bTag;
+    /** @brief */
     std::string m_bTagWPsString;
+    /** @brief */
     std::vector<std::string> m_bTagWPs;
+    /** @brief */
     std::string m_bTagFileName;
+    /** @brief */
     std::string m_bTagVar;
-//    std::string m_bTagOP;
+    /** @brief */
     bool m_useDevelopmentFile;
+    /** @brief */
     bool m_useConeFlavourLabel;
 
     // configuration variables set automatically
+    /** @brief */
     bool m_isMC;                      // Is MC
+    /** @brief */
     std::vector<float> m_subLeadingPtThreshold;
+    /** @brief */
     bool m_useMCPileupCheck;
+    /** @brief */
     bool m_iterateBootstrap;
+    /** @brief */
     std::vector<std::string> m_triggers;
+    /** @brief */
     std::vector<float> m_triggerThresholds;
+    /** @brief */
     std::vector<double> m_bins;
+    /** @brief */
     bool m_VjetCalib;
 
     //config for Jet Tools
+    /** @brief */
     std::string m_jetDef;
+    /** @brief */
     std::string m_jetCalibSequence;
+    /** @brief */
     std::string m_jetCalibConfig;
+    /** @brief */
     std::string m_jetCleanCutLevel;
+    /** @brief */
     bool m_jetCleanUgly;
+    /** @brief */
     float m_JVTCut;
+    /** @brief */
     std::string m_jetUncertaintyConfig;
 
     // for SystTool
+    /** @brief */
     SystContainer  * systTool; //!
+    /** @brief */
     std::vector<double> systTool_ptBins; //!
 
   private:
@@ -151,41 +207,68 @@ class MultijetBalanceAlgo : public EL::Algorithm
 
 
     #ifndef __CINT__
+    /** @brief */
     JetCalibrationTool   * m_JetCalibrationTool;    //!
+    /** @brief */
     JetCleaningTool      * m_JetCleaningTool;       //!
+    /** @brief */
     JetUncertaintiesTool * m_JetUncertaintiesTool;  //!
     #endif // not __CINT__
 
+    /** @brief */
     std::vector< BTaggingSelectionTool* >  m_BJetSelectTools; //!
+    /** @brief */
     std::vector< BTaggingEfficiencyTool* > m_BJetEffSFTools; //!
 
+    /** @brief */
     std::vector< TrigConf::xAODConfigTool* > m_trigConfTools; //!
+    /** @brief */
     std::vector< Trig::TrigDecisionTool* > m_trigDecTools;    //!
 
     //JVTTool
+    /** @brief */
     JetVertexTaggerTool      * m_JVTTool;        //!
+    /** @brief */
     ToolHandle<IJetUpdateJvt>  m_JVTToolHandle;  //!
 
+    /** @brief */
     int m_pvLocation; //!
 
+    /** @brief */
     float m_xs; //!
+    /** @brief */
     float m_acceptance; //!
+    /** @brief */
     int m_numAMIEvents; //!
+    /** @brief */
     int m_mcChannelNumber; //!
+    /** @brief */
     int m_runNumber; //!
+    /** @brief */
     std::stringstream m_ss; //!
 
+    /** @brief */
     std::vector<std::string> m_sysVar; //!
+    /** @brief */
     std::vector<int> m_sysTool; //!
+    /** @brief */
     std::vector<int> m_sysToolIndex; //!
+    /** @brief */
     std::vector<int> m_sysSign; //!
+    /** @brief */
     int m_NominalIndex; //!
 
+    /** @brief */
     std::vector< TH1F* > m_VjetHists; //!
+    /** @brief */
     std::vector< TH1D* > m_MJBHists; //!
+    /** @brief */
     std::map<int, int> m_VjetMap; //!
+    /** @brief */
     std::map<int, int> m_JESMap; //!
+    /** @brief */
     std::vector<std::string> m_JCSTokens; //!
+    /** @brief */
     std::vector<std::string> m_JCSStrings; //!
 
     /** @brief Update every cutflow for this selection
@@ -212,72 +295,99 @@ class MultijetBalanceAlgo : public EL::Algorithm
     unsigned int m_iCutflow;     //!
 
   private:
+    /** @brief */
     double DeltaPhi(double phi1, double phi2);
+    /** @brief */
     double DeltaR(double eta1, double phi1,double eta2, double phi2);
 
 
 
     #ifndef __MAKECINT__
 
-    //MiniTree* m_nominalTree; //!
+    /** @brief */
     std::vector<MiniTree*> m_treeList; //!
+    /** @brief */
     EL::StatusCode getLumiWeights(const xAOD::EventInfo* eventInfo);
+    /** @brief */
     std::vector<MultijetHists*> m_jetHists; //!
 
     #endif
 
-  EL::StatusCode loadVariations();
-  EL::StatusCode loadVariationsOld();
-  EL::StatusCode loadTriggerTool();
-  EL::StatusCode loadJVTTool();
-  EL::StatusCode loadJetCalibrationTool();
-  EL::StatusCode setupJetCalibrationStages();
-  EL::StatusCode loadJetCleaningTool();
-  EL::StatusCode loadJetUncertaintyTool();
-  EL::StatusCode loadVjetCalibration();
-  EL::StatusCode loadMJBCalibration();
-  EL::StatusCode loadBTagTools();
+    /** @brief */
+    EL::StatusCode loadVariations();
+    /** @brief */
+    EL::StatusCode loadVariationsOld();
+    /** @brief */
+    EL::StatusCode loadTriggerTool();
+    /** @brief */
+    EL::StatusCode loadJVTTool();
+    /** @brief */
+    EL::StatusCode loadJetCalibrationTool();
+    /** @brief */
+    EL::StatusCode setupJetCalibrationStages();
+    /** @brief */
+    EL::StatusCode loadJetCleaningTool();
+    /** @brief */
+    EL::StatusCode loadJetUncertaintyTool();
+    /** @brief */
+    EL::StatusCode loadVjetCalibration();
+    /** @brief */
+    EL::StatusCode loadMJBCalibration();
+    /** @brief */
+    EL::StatusCode loadBTagTools();
 
     #ifndef __MAKECINT__
+    /** @brief */
      EL::StatusCode applyJetCalibrationTool( xAOD::Jet* jet);
+    /** @brief */
      EL::StatusCode applyJetCleaningTool();
+    /** @brief */
      EL::StatusCode applyJetUncertaintyTool( xAOD::Jet* jet , int iVar );
+    /** @brief */
      EL::StatusCode applyVjetCalibration( xAOD::Jet* jet , int iVar );
+    /** @brief */
      EL::StatusCode applyMJBCalibration( xAOD::Jet* jet , int iVar, bool isLead = false );
+    /** @brief */
      EL::StatusCode reorderJets(std::vector< xAOD::Jet*>* signalJets);
 
     #endif
 
-  // variables that don't get filled at submission time should be
-  // protected from being send from the submission node to the worker
-  // node (done by the //!)
 public:
-  // Tree *myTree; //!
-  // TH1 *myHist; //!
 
 
 
 
-  // this is a standard constructor
-  MultijetBalanceAlgo (std::string name = "MultijetBalanceAlgo");
-  ~MultijetBalanceAlgo ();
+    /** @brief Standard constructor*/
+    MultijetBalanceAlgo (std::string name = "MultijetBalanceAlgo");
+    /** @brief Standard destructor*/
+    ~MultijetBalanceAlgo ();
 
   // these are the functions inherited from Algorithm
-  virtual EL::StatusCode setupJob (EL::Job& job);
-  virtual EL::StatusCode fileExecute ();
-  virtual EL::StatusCode histInitialize ();
-  virtual EL::StatusCode changeInput (bool firstFile);
-  virtual EL::StatusCode initialize ();
-  virtual EL::StatusCode execute ();
-  virtual EL::StatusCode postExecute ();
-  virtual EL::StatusCode finalize ();
-  virtual EL::StatusCode histFinalize ();
+    /** @brief Setup the job (inherits from Algorithm)*/
+    virtual EL::StatusCode setupJob (EL::Job& job);
+    /** @brief Execute the file (inherits from Algorithm)*/
+    virtual EL::StatusCode fileExecute ();
+    /** @brief Initialize the output histograms before any input file is attached (inherits from Algorithm)*/
+    virtual EL::StatusCode histInitialize ();
+    /** @brief Change to the next input file (inherits from Algorithm)*/
+    virtual EL::StatusCode changeInput (bool firstFile);
+    /** @brief Initialize the input file (inherits from Algorithm)*/
+    virtual EL::StatusCode initialize ();
+    /** @brief Execute each event of the input file (inherits from Algorithm)*/
+    virtual EL::StatusCode execute ();
+    /** @brief End the event execution (inherits from Algorithm)*/
+    virtual EL::StatusCode postExecute ();
+    /** @brief Finalize the input file (inherits from Algorithm)*/
+    virtual EL::StatusCode finalize ();
+    /** @brief Finalize the histograms after all files (inherits from Algorithm)*/
+    virtual EL::StatusCode histFinalize ();
 
   // these are the functions not inherited from Algorithm
-  virtual EL::StatusCode configure ();
+    /** @brief Configure the variables once before running*/
+    virtual EL::StatusCode configure ();
 
-  // this is needed to distribute the algorithm to the workers
-  ClassDef(MultijetBalanceAlgo, 1);
+    /** @brief Used to distribute the algorithm to the workers*/
+    ClassDef(MultijetBalanceAlgo, 1);
 };
 
 #endif
